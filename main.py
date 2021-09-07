9d8c1ca4daa8b5fe1fe96b84d27c4493023116b1
Jagadeesan Muthuvel <jagadeesanmuthuvel@gmail.com>
2021-09-07 15:33:23 +0530

    Added new feature in contact page to receive the POST request data from contact form and send the form data as email

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 BLOG_ENDPOINT='https://api.npoint.io/9dd7cb66e29c10f32d83'
 app = Flask(__name__)
 response = requests.get(BLOG_ENDPOINT)
@@ -7,9 +8,29 @@ blogs = response.json()
 @app.route('/')
 def home():
     return render_template("index.html",blogs=blogs)
-@app.route('/contact')
+
+@app.route('/contact',methods=['GET','POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        phone=request.form['phone']
+        message=request.form['msg']
+        mail_stat=send_mail(name,email,phone,message)
+        print(mail_stat)
+        return render_template('contact.html',mail_st=mail_stat)
+    else:
+        return render_template('contact.html',mail_st=False)
+def send_mail(name,email,phone,message):
+    email_message=f"Subject:New user got enrolled to your blog\n\n Name: {name}\n Email: {email}\n phone:{phone}\nmessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        try:
+            connection.login('your email','youremail password')
+            send=connection.sendmail('your email','your email',email_message)
+            return True
+        except:
+            return False
 
 @app.route('/about')
 def about():
